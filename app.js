12d5fb63ab43fcf162c26e0ecabaa0068810fe23
Md-Alqma <mdalqma7@gmail.com>
2023-08-22 20:47:18 +0530

    used express static to render local files like css and images

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);

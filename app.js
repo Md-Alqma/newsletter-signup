26dabedce2eebc19b99e465fb056915fa0ff2bba
Md-Alqma <mdalqma7@gmail.com>
2023-08-22 21:59:14 +0530

    using mailchimp for email collection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -18,6 +19,43 @@ app.post("/", (req, res) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = `https://us21.api.mailchimp.com/3.0/lists/9b59f0b528`;
+
+  const options = {
+    method: "POST",
+    auth: "alqma:dc08f72588e505f54d1281c8fb9a8658-us21",
+  };
+
+  const request = https.request(url, options, (response) => {
+    response.on("data", (data) => {
+      const parsedData = JSON.parse(data);
+
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(PORT, () => {

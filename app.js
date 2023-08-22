ef596bbc39a319ada2dcd2c9492cb2ad174c0326
Md-Alqma <mdalqma7@gmail.com>
2023-08-22 20:54:29 +0530

    got the data from frontend in the server

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  //   console.log(req.body);
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
 });
 
 app.listen(PORT, () => {
